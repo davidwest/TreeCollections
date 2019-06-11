9ae99a6de5c35095b2ef89f97cafd3ef1db72946
David West <dwest.netdev@gmail.com>
2019-06-10 21:52:15 -0500

    misc doc changes

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,8 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
+
 master_doc = 'index'
 
 # -- Project information -----------------------------------------------------
@@ -42,12 +44,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
